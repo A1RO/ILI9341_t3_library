9653c748da41dfff42fbf73f3d43447150de28fa
A1RO <aone_records@fastmail.com>
2015-11-15 19:11:12 -0500

    Update ILI9341_t3.cpp

--- ILI9341_t3.cpp
+++ ILI9341_t3.cpp
@@ -476,8 +476,6 @@ ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include "glcdfont.c"
-
 // Draw a circle outline
 void ILI9341_t3::drawCircle(int16_t x0, int16_t y0, int16_t r,
     uint16_t color) {
